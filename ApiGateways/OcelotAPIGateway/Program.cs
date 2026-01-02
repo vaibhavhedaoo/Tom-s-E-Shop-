8bd72aa387bad3e4c8050b553562e30a7557962f
vaibhavhedaoo <vaibhavhedaoo12@gmail.com>
2026-01-02 20:56:34 +0530

    changes added

--- ApiGateways/OcelotAPIGateway/Program.cs
+++ ApiGateways/OcelotAPIGateway/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -6,12 +7,26 @@ var builder = WebApplication.CreateBuilder(args);
 if (builder.Environment.IsDevelopment())
 {
     builder.Configuration.AddJsonFile("ocelot.dev.json", optional: false, reloadOnChange: true);
-
 }
 else
 {
     builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 }
+var endpoint = builder.Configuration["Keys:EndPointAuthKey"];
+builder.Services.AddAuthentication().AddJwtBearer(endpoint, option => {     
+   option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+   {
+       ValidateIssuer = false,
+       ValidateAudience = false,
+       ValidateLifetime = true,
+       ValidateIssuerSigningKey = true,
+       ValidIssuer = builder.Configuration["Jwt:Issuer"],
+       ValidAudience  = builder.Configuration["Jwt:Audience"],
+       IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+       RoleClaimType = "Roles"
+   };
+});
+
 builder.Services.AddOcelot();
 var app = builder.Build();
 app.UseOcelot().Wait();
